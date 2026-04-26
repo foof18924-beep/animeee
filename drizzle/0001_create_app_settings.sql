104692d6ce59ccd556d7256b4efde9538bc45b7a
foof18924-beep <foof18924@gmail.com>
2026-04-26 16:49:29 +0100

    Delete drizzle directory

--- drizzle/0001_create_app_settings.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `appSettings` (
-`id` int AUTO_INCREMENT NOT NULL,
-`downloadLink` text NOT NULL,
-`appName` varchar(255) NOT NULL,
-`createdAt` timestamp NOT NULL DEFAULT (now()),
-`updatedAt` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
-CONSTRAINT `appSettings_id` PRIMARY KEY(`id`)
-);
